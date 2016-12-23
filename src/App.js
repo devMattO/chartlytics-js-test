b9d22eb7c06c6e76ea6256a06979bc0945be0691
Matt Olsen <olsen_matthew@yahoo.com>
2016-12-22 16:42:29 -0800

    added input data to app js file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+
+const resources = [{
+        type: 'Person',
+        order: 1,
+        name: 'Brian',
+      }, {
+        type: 'Place',
+        order: 2,
+        name: 'Ohio',
+      }, {
+        type: 'Place',
+        order: 12,
+        name: 'Ohio',
+      }, {
+        type: 'Person',
+        order: 14,
+        name: 'Sarah',
+      },  {
+        type: 'Person',
+        order: 199,
+        name: 'Sam',
+      }, {
+        type: 'Person',
+        order: 19,
+        name: 'Eric',
+      }, {
+        type: 'Place',
+        order: 20,
+        name: 'Home',
+      }
+    ]
 
 class App extends Component {
   render() {
+    console.log(resources,'<----resources');
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Chartlytics JS Test</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div id="outer-container">
+          {resources.map((el,index)=>{
+            console.log(el)
+          })}
+        </div>
       </div>
     );
   }
